109f357f5868afb56279f87028dd668c06ba5db9
yam t <yam.tirosh@gmail.com>
2022-03-27 13:51:51 +0300

    Backend code: - Add pymongo requirement - Medical DAL - API

---
+++ src/backend/tmr/src/dal.py
@@ -0,0 +1,11 @@
+from dataclasses import dataclass
+
+from pymongo.collection import Collection
+
+
+@dataclass
+class MedicalDal:
+    patients: Collection
+
+    def patient_count_in_wing(self, wing_id: int) -> int:
+        return self.patients.count_documents({"wing_id": wing_id})
