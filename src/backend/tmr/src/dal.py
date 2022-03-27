d45d9022fdd5d26813862fbdccadef69c2ad110e
shayel baruch <shayelbaruch@gmail.com>
2022-03-27 22:57:49 +0300

    added json and bson correct decoder

--- src/backend/tmr/src/dal.py
+++ src/backend/tmr/src/dal.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 from bson.objectid import ObjectId
 from bson.json_util import dumps
+import json
 from pymongo.database import Database
 
 
@@ -12,12 +13,10 @@ class MedicalDal:
         return self.db.patients.count_documents({"wing_id": ObjectId(wing_id)})
 
     def patients_in_wing(self, wing_id: str) -> dict:
-        for patient in self.db.patients.find({"wing_id": ObjectId(wing_id)}, {"bed": 1}):
-            yield dict(_id=patient.pop('_id').binary.hex(), **patient)
+        return json.loads(dumps(self.db.patients.find({"wing_id": ObjectId(wing_id)}, {"bed": 1})))
 
     def get_wing_details(self, wing_id: str) -> dict:
-        wing = self.db.wings.find_one({"_id": ObjectId(wing_id)})
-        return dict(_id=wing.pop('_id').binary.hex(), **wing)
+        return json.loads(dumps(self.db.wings.find_one({"_id": ObjectId(wing_id)})))
 
     def get_patient_info_by_id(self, patient_id: str) -> dict:
-        return dumps(self.db.patients.find_one({"_id": ObjectId(patient_id)}))
+        return json.loads(dumps(self.db.patients.find_one({"_id": ObjectId(patient_id)})))
