3e55ed8fae5a4e1790e2cffe84fbe19b46fd0584
shayel baruch <shayelbaruch@gmail.com>
2022-03-27 19:54:21 +0300

    get data of patient in wing and wing details

--- src/backend/tmr/src/dal.py
+++ src/backend/tmr/src/dal.py
@@ -1,14 +1,18 @@
 from dataclasses import dataclass
-
-from pymongo.collection import Collection
+from bson.objectid import ObjectId
+from bson.json_util import dumps
+from pymongo.database import Database
 
 
 @dataclass
 class MedicalDal:
-    patients: Collection
+    db: Database
 
-    def patient_count_in_wing(self, wing_id: int) -> int:
-        return self.patients.count_documents({"wing_id": wing_id})
+    def patient_count_in_wing(self, wing_id: str) -> int:
+        return self.db.patients.count_documents({"wing_id": ObjectId(wing_id)})
 
-    def patients_in_wing(self, wing_id: int) -> dict:
-        return self.patients.find({"wing_id": wing_id})
+    def patients_in_wing(self, wing_id: str) -> dict:
+        return dumps(self.db.patients.find({"wing_id": ObjectId(wing_id)}))
+
+    def get_wing_details(self, wing_id: str) -> dict:
+        return dumps(self.db.wings.find_one({"_id": ObjectId(wing_id)}))
--- src/backend/tmr/wing.py
+++ src/backend/tmr/wing.py
@@ -7,14 +7,19 @@ wing_router = APIRouter()
 
 
 def medical_dal() -> MedicalDal:
-    return MedicalDal(MongoClient("localhost").get_database("tmr").get_collection("patients"))
+    return MedicalDal(MongoClient("medical-db").tmr)
 
 
 @wing_router.get("/{wing_id}/patient_count")
-def patient_count(wing_id: int, dal: MedicalDal = Depends(medical_dal)) -> int:
+def patient_count(wing_id: str, dal: MedicalDal = Depends(medical_dal)) -> int:
     return dal.patient_count_in_wing(wing_id)
 
 
-@wing_router.get("/{wing_id}")
-def patients_in_wing(wing_id: int, dal: MedicalDal = Depends(medical_dal)) -> dict:
+@wing_router.get("/{wing_id}/patients")
+def patients_in_wing(wing_id: str, dal: MedicalDal = Depends(medical_dal)) -> dict:
     return dal.patients_in_wing(wing_id)
+
+
+@wing_router.get("/{wing_id}/details")
+def wing_details(wing_id: str, dal: MedicalDal = Depends(medical_dal)) -> dict:
+    return dal.get_wing_details(wing_id)
