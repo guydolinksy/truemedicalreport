7c58479504079fc7c484f6c722d4306affe18f5b
Guy Dolinsky <guydolinsky@gmail.com>
2022-03-28 21:07:45 +0300

    Add data websocket refresh
    Add react livereload websocket

--- src/frontend/tmr/src/components/Patient.js
+++ src/frontend/tmr/src/components/Patient.js
@@ -9,63 +9,73 @@ import {
     faUserNurse,
     faWarning,
 } from "@fortawesome/free-solid-svg-icons";
-import {CheckOutlined, UserOutlined, AlertOutlined, FlagOutlined} from '@ant-design/icons'
+import {AlertOutlined, CheckOutlined, FlagOutlined, UserOutlined} from '@ant-design/icons'
 import Axios from 'axios';
+import useWebSocket from "react-use-websocket";
 
-const PatientDataContext = React.createContext();
+const Data = React.createContext(null);
 
-const deepReplace = (path, data, value) => {
-    if (!path.length) {
-        return value
-    }
-    const key = path.pop()
-    return Object.assign({}, data, {[key]: deepReplace(path, data[key], value)});
-}
-const withPatientData = Component => ({id, bed, data, ...props}) => {
-    const [patientData, setPatientData] = useState({loading: true});
+const DataContext = ({url, updateURL, socketURL, defaultValue, fetchOnMount, ...props}) => {
+    const [{loadingData, value}, setValue] = useState({loading: false, value: defaultValue});
+    const {lastMessage} = useWebSocket(`wss://${window.location.host}/api/ws`, {queryParams: {url: socketURL}});
 
     useEffect(() => {
-        if (data) {
-            setPatientData(data);
-            return;
-        }
-        Axios.get(bed ? `/api/patient/bed/${bed}` : `/api/patient/id/${id}`).then(response => {
-            setPatientData(Object.assign({loading: false}, response.data));
+        if (!fetchOnMount) return;
+        Axios.get(url).then(response => {
+            setValue({loading: false, value: response.data});
         }).catch(error => {
             console.error(error)
         })
+    }, [url, fetchOnMount, lastMessage]);
 
-    }, [bed, id, data]);
-    const updatePatientData = useCallback((path, value) => {
-        setPatientData(prevState => deepReplace(path.slice().reverse(), prevState, value));
-    }, []);
-    return <PatientDataContext.Provider value={{patientData: patientData, updatePatientData: updatePatientData}}>
-        <Component id={id} bed={bed} patientData={patientData} updatePatientData={updatePatientData} {...props}/>
-    </PatientDataContext.Provider>
+    const getData = useCallback((path) =>
+            path.reduce((data, name) => data ? data[name] : undefined, value),
+        [value]);
+
+    const updateData = useCallback((path, newValue) => {
+        const deepReplace = (path, data, value) => {
+            if (!path.length) {
+                return value
+            }
+            const key = path.pop()
+            return Object.assign({}, data, {[key]: deepReplace(path, data[key], value)});
+        }
+        setValue(prevState => {
+            const newData = deepReplace(path.slice().reverse(), prevState.value, newValue)
+            Axios.post(updateURL, {data: newData, path: path, value: newValue}).catch(error => {
+                console.error(error)
+            });
+            return {loading: prevState.loading, value: newData}
+        });
+    }, [updateURL]);
+
+    return <Data.Provider value={{getData: getData, updateData: updateData, loadingData: loadingData}}>
+        {props.children({getData: getData, updateData: updateData, loadingData: loadingData, ...props})}
+    </Data.Provider>
 }
-const PatientData = ({path, hover, icon, size, editable}) => {
+const withData = Component => ({...props}) => {
+    return <Data.Consumer>{({getData, updateData, loadingData}) =>
+        <Component loadingData={loadingData} getData={getData} updateData={updateData} {...props}/>
+    }</Data.Consumer>
+};
+
+const PatientData = withData(({path, hover, icon, size, editable, loadingData, getData, updateData}) => {
     const [editing, setEditing] = useState(false)
 
-
-    return <PatientDataContext.Consumer>
-        {({patientData, updatePatientData}) => {
-            const value = path.reduce((data, name) => data ? data[name] : undefined, patientData);
-            const content = <span style={{userSelect: "none"}}><FontAwesomeIcon icon={icon}/>&nbsp;{value}</span>;
-            return <Tooltip overlay={hover}>
-                {!editable ? content : !editing ?
-                    <Button type={"text"} onClick={() => setEditing(true)}>{content}</Button> :
-                    <Input.Group compact>
-                        <Input size={size} style={{width: 150}}
-                               value={value}
-                               onChange={e => updatePatientData(path, e.target.value)}
-                               onPressEnter={() => setEditing(false)}
-                        />
-                        <Button type="primary" size={size} onClick={() => setEditing(false)} icon={<CheckOutlined/>}/>
-                    </Input.Group>}
-            </Tooltip>
-        }}
-    </PatientDataContext.Consumer>
-}
+    const content = <span style={{userSelect: "none"}}><FontAwesomeIcon icon={icon}/>&nbsp;{getData(path)}</span>;
+    return <Tooltip overlay={hover}>
+        {!editable ? content : !editing ?
+            <Button type={"text"} onClick={() => setEditing(true)}>{content}</Button> :
+            <Input.Group compact>
+                <Input size={size} style={{width: 150}}
+                       value={getData(path)}
+                       onChange={e => updateData(path, e.target.value)}
+                       onPressEnter={() => setEditing(false)}
+                />
+                <Button type="primary" size={size} onClick={() => setEditing(false)} icon={<CheckOutlined/>}/>
+            </Input.Group>}
+    </Tooltip>
+});
 
 
 const dataItems = [
@@ -75,60 +85,65 @@ const dataItems = [
     {path: ['esiScore'], icon: faUserNurse, hover: 'ערך ESI אחרון', showMinimized: true},
 ]
 
-export const Patient = withPatientData(({bed, patientData, updatePatientData}) => {
-    let avatar;
-    if ((patientData.warnings || []).length)
-        avatar = <Button shape={"circle"} type={"primary"} icon={<AlertOutlined/>} danger/>
-    else if (patientData.flagged)
-        avatar = <Button shape={"circle"} type={"primary"} icon={<FlagOutlined/>} danger
-                         onClick={() => updatePatientData(['flagged'], false)}/>
-    else if (bed)
-        avatar = <Button shape={"circle"} type={"primary"}
-                         onClick={() => updatePatientData(['flagged'], true)}>{bed}</Button>
-    else
-        avatar = <Button shape={"circle"} type={"primary"} icon={<UserOutlined/>}
-                         onClick={() => updatePatientData(['flagged'], true)}/>;
-
-    const title = <span>{avatar}&nbsp;{patientData.name}</span>
-    const actions = dataItems.filter(({path, showMinimized}) => (bed || showMinimized)).map(
-        ({path, icon, hover}) =>
-            <PatientData editable={false} icon={icon} path={path} hover={hover}/>
-    );
-    const extra = <PatientData editable size={"small"} icon={faClock} hover={'ממתין.ה עבור'} path={["awaiting"]}/>
-
-
+export const Patient = ({bed, id, defaultData}) => {
     const slider = useRef(null);
+    const uri = bed ? `/api/patient/bed/${bed}` : `/api/patient/id/${id}`
+    return <DataContext url={uri} updateURL={uri} socketURL={uri} fetchOnMount defaultValue={defaultData}>
+        {({loadingData, getData, updateData}) => {
+            let avatar;
+            if ((getData(['warnings']) || []).length)
+                avatar = <Button shape={"circle"} type={"primary"} icon={<AlertOutlined/>} danger/>
+            else if (getData(['flagged']))
+                avatar = <Button shape={"circle"} type={"primary"} icon={<FlagOutlined/>} danger
+                                 onClick={() => updateData(['flagged'], false)}/>
+            else if (bed)
+                avatar = <Button shape={"circle"} type={"primary"}
+                                 onClick={() => updateData(['flagged'], true)}>{bed}</Button>
+            else
+                avatar = <Button shape={"circle"} type={"primary"} icon={<UserOutlined/>}
+                                 onClick={() => updateData(['flagged'], true)}/>;
 
-    return <Card type={"inner"} size={"small"} headStyle={{backgroundColor: "#e6fffb"}} bodyStyle={{padding: 0}}
-                 style={{marginBottom: 16}}
-                 title={title} actions={actions} extra={extra}>
-        <Carousel ref={slider} autoplay dotPosition={"top"}>
-            <div>
-                <div style={{
-                    userSelect: "none",
-                    padding: 20,
-                    backgroundColor: "#fff7e6",
-                    textAlign: "center",
-                    cursor: (patientData.warnings || []).length ? "pointer" : undefined
-                }}
-                     onClick={() => slider.current.next()}>
-                    {patientData.complaint}
-                </div>
-            </div>
-            {(patientData.warnings || []).map((warning, i) =>
-                <div>
-                    <div key={i}
-                         style={{
-                             userSelect: "none",
-                             padding: 20,
-                             backgroundColor: "#ffccc7",
-                             textAlign: "center",
-                             cursor: "pointer"
-                         }}
-                         onClick={() => slider.current.next()}>
-                        {warning}&nbsp;<FontAwesomeIcon icon={faWarning}/>
+            const title = <span>{avatar}&nbsp;{getData(['name'])}</span>
+            const actions = dataItems.filter(({path, showMinimized}) => (bed || showMinimized)).map(
+                ({path, icon, hover}) =>
+                    <PatientData editable={false} icon={icon} path={path} hover={hover}/>
+            );
+            const extra = <PatientData editable size={"small"} icon={faClock} hover={'ממתין.ה עבור'}
+                                       path={["awaiting"]}/>
+
+            return <Card type={"inner"} size={"small"} headStyle={{backgroundColor: "#e6fffb"}}
+                         bodyStyle={{padding: 0}}
+                         style={{marginBottom: 16}}
+                         title={title} actions={actions} extra={extra}>
+                <Carousel ref={slider} autoplay dotPosition={"top"}>
+                    <div>
+                        <div style={{
+                            userSelect: "none",
+                            padding: 20,
+                            backgroundColor: "#fff7e6",
+                            textAlign: "center",
+                            cursor: (getData(['warnings']) || []).length ? "pointer" : undefined
+                        }}
+                             onClick={() => slider.current.next()}>
+                            {getData(['complaint'])}
+                        </div>
                     </div>
-                </div>)}
-        </Carousel>
-    </Card>
-});
+                    {(getData(['warnings']) || []).map((warning, i) =>
+                        <div key={i}>
+                            <div
+                                style={{
+                                    userSelect: "none",
+                                    padding: 20,
+                                    backgroundColor: "#ffccc7",
+                                    textAlign: "center",
+                                    cursor: "pointer"
+                                }}
+                                onClick={() => slider.current.next()}>
+                                {warning}&nbsp;<FontAwesomeIcon icon={faWarning}/>
+                            </div>
+                        </div>)}
+                </Carousel>
+            </Card>
+        }}
+    </DataContext>
+};
