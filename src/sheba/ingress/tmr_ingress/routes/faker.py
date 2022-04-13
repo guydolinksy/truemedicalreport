79b8bf7310aba746b6828c8cce024a5e35c9a988
Negev Asaf <negevasaf@gmail.com>
2022-04-13 13:46:35 +0300

    updated faker route

--- src/sheba/ingress/tmr_ingress/routes/faker.py
+++ src/sheba/ingress/tmr_ingress/routes/faker.py
@@ -5,7 +5,7 @@ from src.sheba.ingress.tmr_ingress.faker.fake_data.fake_main import FakeMain
 
 faker_router = APIRouter()
 
-@faker_router.get("/{patient}/measurement", tags=["Patient"])
+@faker_router.get("/faker/all", tags=["Patient"])
 def update_measurements():
     """
     update the measurments of a single patient.
