b2ad745056fadd2bab9f3fbd4160ac4168e1f728
nitzan sasson <nitzan.sasson@gmail.com>
2022-04-27 11:47:56 +0300

    updated imaging class to dal

--- src/sheba/ingress/tmr_ingress/models/imaging.py
+++ src/sheba/ingress/tmr_ingress/models/imaging.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Float, VARCHAR, Integer, Column, DateTime
 from sqlalchemy.orm import declarative_base
+from tmr_common.data_models.imaging import Imaging
 
 Base = declarative_base()
 
@@ -11,4 +12,13 @@ class Imaging(Base):
     imaging_id = Column("imaging_id", Integer())
     imaging_name = Column("imaging_name", VARCHAR(60))
     result = Column("result_name", VARCHAR(100))
+    link = Column("link", VARCHAR(100))
     date = Column("result_date", DateTime())
+
+    def to_dal(self):
+        return Imaging(
+            at=self.at.isoformat(),
+            name=self.imaging_name,
+            value=self.result,
+            link=self.link,
+        )
