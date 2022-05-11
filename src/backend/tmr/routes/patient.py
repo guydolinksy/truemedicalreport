39374b522c167f0e3f5a5e5f1ac4fd2a1b360b82
Amit Haim <haamitac@hotmail.co.il>
2022-05-11 21:08:14 +0300

    Added low pulse warning (below 60)

--- src/backend/tmr/routes/patient.py
+++ src/backend/tmr/routes/patient.py
@@ -1,10 +1,12 @@
+import datetime
 from json import loads
 
 import requests
 from fastapi import APIRouter, Body
 from werkzeug.exceptions import NotFound
 
-from tmr_common.data_models.patient import Patient, PatientInfo
+from tmr_common.data_models.patient import Patient, PatientInfo, Warning
+from tmr_common.data_models.severity import Severity
 from ..utils import prepare_update_object
 
 patient_router = APIRouter()
@@ -13,7 +15,12 @@ patient_router = APIRouter()
 @patient_router.get("/{patient}")
 def get_patient_by_id(patient: str) -> dict:
     res = requests.get(f"http://medical-dal/medical-dal/patients/{patient}").json()
-    return Patient(**res).dict() if res else None
+    requested_patient = Patient(**res)
+    if requested_patient.measures.pulse is not None and int(requested_patient.measures.pulse.value) < 60:
+        severity = Severity(value=2, at=datetime.datetime.utcnow().isoformat())
+        requested_patient.warnings.append(Warning(content="דופק נמוך", severity=severity))
+
+    return requested_patient.dict() if res else None
 
 
 @patient_router.get("/{patient}/info")
