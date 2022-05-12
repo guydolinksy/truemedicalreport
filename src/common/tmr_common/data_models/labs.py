a4ce0cf115534613cb532efb99465d6de4b4eadb
Negev Asaf <negevasaf@gmail.com>
2022-05-12 19:22:52 +0300

    Finished labs notification

--- src/common/tmr_common/data_models/labs.py
+++ src/common/tmr_common/data_models/labs.py
@@ -54,8 +54,10 @@ class LabTest(BaseModel):
 class LabsResultsInCategory(BaseModel):
     category_id: str
     category_results: list[LabTest]
+
     class Config:
         orm_mode = True
+
 
 class LabsResultsOfPatient(BaseModel):
     patient_id: Optional[int]
@@ -65,9 +67,24 @@ class LabsResultsOfPatient(BaseModel):
     class Config:
         orm_mode = True
 
-    #TODO: Plan wanted notification
+    # TODO: Plan wanted notification
     def to_notification(self):
         return LabsNotification(
             static_id=self.external_id,
             patient_id=self.patient_id,
+            at=datetime.now(),
+            message = self.get_notification_message()
         )
+
+    def _get_categories_name(self) -> []:
+        categories_names = []
+        for category_data in self.lab_results:
+            categories_names.append(CategoriesInHebrew[LabsCategories[category_data.category_id]])
+        return categories_names
+
+    def get_notification_message(self)->str:
+        categories_names = self._get_categories_name()
+        message = "התקבלו תוצאות מעבדה חדשות ב-"
+        for category in categories_names:
+            message += f" {category},"
+        return message
