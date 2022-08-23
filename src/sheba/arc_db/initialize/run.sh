27c5f14a6862ee53d15155f002e5f48afce4b038
tamar.bensimon <tamar.bensimon@belong.life>
2022-08-23 23:08:41 +0300

    fix db script

--- src/sheba/arc_db/initialize/run.sh
+++ src/sheba/arc_db/initialize/run.sh
@@ -1,7 +1,7 @@
 TIMEOUT=90
 for i in $(seq 1 $TIMEOUT);
   do
-    /opt/mssql-tools/bin/sqlcmd -S localhost,1433 -U sa -P $SA_PASSWORD -d master -i /opt/arc_db/initialize/arc_db.sql
+    /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -d master -i /opt/arc_db/initialize/arc_db.sql
     if [ $? -eq 0 ]
     then
         break
