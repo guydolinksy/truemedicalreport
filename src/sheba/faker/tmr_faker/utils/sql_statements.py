3540171b0b8aab9dda1a3fec7633757de9bcdafe
tamar.bensimon <tamar.bensimon@belong.life>
2022-08-30 23:59:31 +0300

    fix truncate and faker procedures

--- src/sheba/faker/tmr_faker/utils/sql_statements.py
+++ src/sheba/faker/tmr_faker/utils/sql_statements.py
@@ -1,11 +1,11 @@
-execute_set_patient_admission = """execute dwh.dbo.faker_RoomPlacmentPatient_admission {}"""
+execute_set_patient_admission = """execute dwh.dw.faker_RoomPlacmentPatient_admission {}"""
 
-execute_set_hospitalized_decision = """execute dwh.dbo.faker_decision {}"""
+execute_set_hospitalized_decision = """execute dwh.dw.faker_decision {}"""
 
-execute_set_responsible_doctor = """execute dwh.dbo.faker_ResponsibleDoctor {}"""
+execute_set_responsible_doctor = """execute dwh.dw.faker_ResponsibleDoctor {}"""
 
-delete_room_placement =""" truncate table sbwnd81c.chameleon.dbo.RoomPlacementPatient"""
+delete_room_placement = """ EXEC sbwnd81c.chameleon.sys.sp_executesql N'TRUNCATE TABLE  dbo.RoomPlacementPatient' """
 
-delete_admission_treatment_decision = """truncate table sbwnd81c.chameleon.dbo.AdmissionTreatmentDecision"""
+delete_admission_treatment_decision = """EXEC sbwnd81c.chameleon.sys.sp_executesql N'TRUNCATE TABLE  dbo.AdmissionTreatmentDecision' """
 
-delete_responsible_doctor= """truncate table sbwnd81c.chameleon.dbo.ResponsibleDoctor"""
+delete_responsible_doctor = """EXEC sbwnd81c.chameleon.sys.sp_executesql N'TRUNCATE TABLE  dbo.ResponsibleDoctor' """
