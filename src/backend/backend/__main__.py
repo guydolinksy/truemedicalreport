8d5cecfc876b643ceb65895d52f346aadef80c00
shayel baruch <shayelbaruch@gmail.com>
2022-12-14 12:03:35 +0200

    fix faker running configuraion

--- src/backend/backend/__main__.py
+++ src/backend/backend/__main__.py
@@ -3,7 +3,7 @@ import uvicorn
 
 env = environs.Env()
 
-debug = env.bool("TMR_BACKEND_DEBUG")
+debug = env.bool("DEBUG")
 port = env.int("TMR_BACKEND_PORT", 80)
 
 uvicorn.run("backend.app:app", host="0.0.0.0", port=port, debug=debug)
