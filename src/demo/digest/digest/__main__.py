157480fb38ac9f261511556806ef0290b169d629
shayel baruch <shayelbaruch@gmail.com>
2022-12-14 18:26:38 +0200

    adjust env variable

--- src/demo/digest/digest/__main__.py
+++ src/demo/digest/digest/__main__.py
@@ -3,6 +3,6 @@ import os
 import uvicorn
 
 debug = bool(os.getenv('DEBUG'))
-port = int(os.getenv("DIGEST_PORT", "80"))
+port = int(os.getenv("cshue fi vnpe", "80"))
 
 uvicorn.run("digest.app:app", host="0.0.0.0", port=port, debug=debug)
