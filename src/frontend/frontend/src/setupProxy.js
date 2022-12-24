e47189a27f4fde8da9031d5c83f7ba5ca8435ac5
Matan Rosenberg <matan.rosenberg@forter.com>
2022-12-24 23:49:36 +0200

    Fix WS dev proxy

--- src/frontend/frontend/src/setupProxy.js
+++ src/frontend/frontend/src/setupProxy.js
@@ -5,19 +5,22 @@
  Sometimes we're running the proxy server locally, without k8s ingress / nginx
  */
 
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const {createProxyMiddleware} = require('http-proxy-middleware');
 
-module.exports = function(app) {
+module.exports = function (app) {
     const proxy_port = process.env.REACT_APP_TMR_PROXY_BACKEND_TO_PORT;
 
     if (proxy_port) {
         console.info(`Proxying api to port ${proxy_port}`)
-        app.use(
-            '/api',
-            createProxyMiddleware({
-                target: `http://127.0.0.1:${proxy_port}`,
-                changeOrigin: true,
-            })
-        );
+        const apiBaseUrl = `http://127.0.0.1:${proxy_port}`;
+
+        app.use(createProxyMiddleware('/api', {
+            target: apiBaseUrl,
+        }));
+
+        app.use(createProxyMiddleware("/api/sync", {
+            target: apiBaseUrl,
+            ws: true,
+        }))
     }
 };
