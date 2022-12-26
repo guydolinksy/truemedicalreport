5e9d03ffe06e70a27f75ee42c38aec43969974c3
Amit Haim <haamitac@hotmail.co.il>
2022-12-26 23:09:44 +0200

    Added fallback if running without helm

--- src/backend/backend/__main__.py
+++ src/backend/backend/__main__.py
@@ -7,7 +7,7 @@ env = environs.Env()
 debug = env.bool("DEBUG")
 port = env.int("PORT", 80)
 
-sentry_dsn = env.str("SENTRY_CONNECTION_DSN")
+sentry_dsn = env.str("SENTRY_CONNECTION_DSN", "")
 sentry_sdk.init(sentry_dsn)
 
 uvicorn.run("backend.app:app", host="0.0.0.0", port=port, debug=debug)
