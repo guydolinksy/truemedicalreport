ffff27ae126c5cc40c38d6ea4ae5047b08828998
Matan Rosenberg <matan.rosenberg@forter.com>
2023-01-18 20:37:05 +0200

    Sentry-lize SQLAlchemy @ digest

--- src/sheba/digest/digest/app.py
+++ src/sheba/digest/digest/app.py
@@ -1,13 +1,19 @@
 import sys
 
-import sentry_sdk
 from fastapi_offline import FastAPIOffline
 from logbook import StreamHandler
+from common.tracing import setup_sentry
+from sentry_sdk.integrations.sqlalchemy import SqlalchemyIntegration
 
 from . import config
 
 
 def create_app() -> FastAPIOffline:
+    if config.sentry_dsn:
+        setup_sentry(config.sentry_dsn, extra_integrations=[
+            SqlalchemyIntegration()
+        ])
+
     app_ = FastAPIOffline(openapi_url='/digest/openapi.json', static_url="/digest/static-offline-docs",
                           docs_url='/digest/docs')
 
@@ -18,9 +24,6 @@ def create_app() -> FastAPIOffline:
 
     from .logics.startup import startup
     app_.on_event('startup')(startup)
-
-    if config.sentry_dsn:
-        sentry_sdk.init(config.sentry_dsn)
 
     return app_
 
