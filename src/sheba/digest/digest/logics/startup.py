88fdf7ecb5380572c7ea7c449253f45bc9229e77
benilkashi <ben.ilkasshi@gmail.com>
2023-07-30 18:30:34 +0300

    Add new protocols

--- src/sheba/digest/digest/logics/startup.py
+++ src/sheba/digest/digest/logics/startup.py
@@ -114,15 +114,19 @@ async def init_protocols():
                     'imaging-צילום בית החזה בשכיבה/ישיבה',
                     'imaging-צילום חזה+צבס',
                 ]
-    TRPONIN_KEYS = ['lab-120104040']
     HCT_KEYS = [
                     'imaging-CT מוח לבירור שבץ-מוחי',
                     'imaging-CT מוח',
                     'imaging-CTA פרוטוקול פרפוזיה כולל מח ואנגיו צוואר ומוח'
                 ]
+    US_STOMACH_KEYS = ['imaging-US בטן']
+    TRPONIN_KEYS = ['lab-120104040']
     HGB_KEYS = ['lab-100109497']
     CRP_KEYS = ['lab-120104016']
     WBC_KEYS = ['lab-100109500']
+    TEMPERATURE_KEYS = ['measure-temperature']
+    SATURATION_KEYS = ['measure-saturation']
+
     requests.post(f'{config.dal_url}/config/set', json={
         'key': 'protocols',
         'version': 1,
@@ -148,6 +152,9 @@ async def init_protocols():
                 {'name': 'צילום אגן', 'default': 'לא הוזמן', 'key': 'img-2', 'keys': ['imaging-צילום אגן/אגן וירך']},
                 {'name': 'צילום חזה', 'default': 'לא הוזמן', 'key': 'img-3', 'keys': CXR_KEYS},
             ],
+            'דימום מדרכי עיכול': [
+                {'name': 'המוגלובין', 'default': '-', 'key': 'lab-1', 'keys': HGB_KEYS + ['lab-100107943']},
+            ],
             'לאחר פרכוס': [
                 {'name': 'CT מוח', 'default': 'לא הוזמן', 'key': 'img-1', 'keys': HCT_KEYS},
                 {'name': 'Total CPK', 'default': '-', 'key': 'lab-1', 'keys': ['lab-100182550']},
@@ -159,9 +166,25 @@ async def init_protocols():
                 {'name': 'hco3', 'default': '-', 'key': 'lab-3', 'keys': ['lab-152808697']},
                 {'name': 'טרופונין', 'default': '-', 'key': 'lab-4', 'keys': TRPONIN_KEYS},
             ],
+            'תלונות א.א.ג': [
+                {'name': 'WBC', 'default': '-', 'key': 'lab-1', 'keys': WBC_KEYS},
+                {'name': 'CRP', 'default': '-', 'key': 'lab-2', 'keys': CRP_KEYS},
+                {'name': 'חום', 'default': '-', 'key': 'measure-1', 'keys': TEMPERATURE_KEYS},
+                {'name': 'סטורציה', 'default': '-', 'key': 'measure-2', 'keys': SATURATION_KEYS},
+            ],
+            'חשד לבקע': [
+                {'name': 'US כליות ודרכי שתן', 'default': 'לא הוזמן', 'key': 'img-1', 'keys': ['imaging-US כליות ודרכי שתן']},
+                {'name': 'US בטן', 'default': 'לא הוזמן', 'key': 'img-2', 'keys': US_STOMACH_KEYS},
+                {'name': 'US ורידי הגפיים התחתונות כולל דופלר', 'default': 'לא הוזמן', 'key': 'img-2', 'keys': ['imaging-US ורידי הגפיים התחתונות כולל דופלר']},
+            ],
             'סחרחורת': [
                 {'name': 'CT מוח', 'default': 'לא הוזמן', 'key': 'img-1', 'keys': HCT_KEYS},
                 {'name': 'HGB', 'default': '-', 'key': 'lab-1', 'keys': HGB_KEYS},
+            ],
+            'כאב בטן': [
+                {'name': 'WBC', 'default': '-', 'key': 'lab-1', 'keys': WBC_KEYS},
+                {'name': 'CRP', 'default': '-', 'key': 'lab-2', 'keys': CRP_KEYS},
+                {'name': 'US בטן', 'default': 'לא הוזמן', 'key': 'img-1', 'keys': US_STOMACH_KEYS},
             ],
             'תלונות לאחר ניתוח': [
                 {'name': 'WBC', 'default': '-', 'key': 'lab-1', 'keys': WBC_KEYS},
@@ -172,6 +195,9 @@ async def init_protocols():
                 {'name': 'CT מוח', 'default': 'לא הוזמן', 'key': 'img-1', 'keys': HCT_KEYS},
                 {'name': 'WBC', 'default': '-', 'key': 'lab-1', 'keys': WBC_KEYS},
                 {'name': 'CRP', 'default': '-', 'key': 'lab-2', 'keys': CRP_KEYS},
+            ],
+            'תאונת דרכים': [
+                {'name': 'CT עמוד שדרה צוארי', 'default': 'לא הוזמן', 'key': 'img-1', 'keys': ['CT עמוד שדרה צוארי']},
             ],
         }
     }).raise_for_status()
