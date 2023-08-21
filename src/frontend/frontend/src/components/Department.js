4fa469cd2a9e3ae97d98ec0882043198b31339ed
Yodem <yotamfromm123@gmail.com>
2023-08-21 18:26:32 +0300

    almost finished

--- src/frontend/frontend/src/components/Department.js
+++ src/frontend/frontend/src/components/Department.js
@@ -11,10 +11,18 @@ import {loginContext} from "./LoginContext";
 import Axios from "axios";
 
 const SHOW_ACTIONS = ['not-awaiting', 'doctor.exam', 'nurse.exam', 'imaging', 'laboratory', 'treatment.ללא'];
-const hideSetting = (e,setting) => {
-        if (e.stopPropagation) e.stopPropagation();
-    console.log(setting)
-    Axios.post('/api/settings/statistics', {values:setting}).then(() => {
+const hideSetting = (e,setting,userSettings,userName,wing) => {
+    console.log(wing)
+    const prevSettings = userSettings?.statistics[wing] || []
+    if (e.stopPropagation) e.stopPropagation();
+   // const propertyName = `${userName}.display.statistics.${wing}`;
+
+    const updateData = {
+        statistics: {
+      [wing]: !!prevSettings.length ? [].concat([...prevSettings]).concat([[`${setting}`, "False"]]) : [[`${setting}`, "False"]]
+        }
+    };
+    Axios.post('/api/settings/display', updateData).then(() => {
         console.log("success")
         document.location.reload();
         }).catch(error => {
@@ -29,7 +37,7 @@ const departmentDataContext = createContext(null);
 export const Department = ({department}) => {
     const uri = `/api/departments/${department}`;
     const navigate = useNavigate();
-    const {userSettings} = useContext(loginContext);
+    const {userSettings,user} = useContext(loginContext);
 
     return <departmentDataContext.Provider url={uri} defaultValue={{wings: []}}>
         {({loading, value}) => <Row gutter={16}>
@@ -48,14 +56,21 @@ export const Department = ({department}) => {
                     ...wing.filters.treatments.map(toActions),
                     ...wing.filters.time_since_arrival.map(toActions),
                 ).filter(({key}) => {
-                    const filters = [].concat(userSettings.statistics[wing.oid] || [], userSettings.statistics.default || []);
-                    if (filters.some(([regex, result]) => result && key.match(regex)))
+                    const filters = [].concat(userSettings.statistics[wing.details.key] || [], userSettings.statistics.default || []);
+                    console.log({filters})
+                    if (filters.some(([regex, result]) => {
+                        console.log({regex, result, key})
+                        return result && (key.match(regex) || key === regex)
+                    }))
                         return true
-                    return !filters.some(([regex, result]) => !result && key.match(regex));
+                    return !filters.some(([regex, result]) => {
+                        console.log({regex, result, key})
+                        return !result && (key.match(regex) || key === regex)
+                    });
                 }).map(
                     ({count, title, icon, duration, valid, key}) => <div
                         style={{display:"flex",flexDirection:"column", alignItems:"center", padding:5,border:"1px solid $f0f0f0"}}>
-                        <div className="departmentActionItem" onClick={(e)=>{hideSetting(e,key)}} style={{alignSelf: "start"}}>{<CloseCircleOutlined />}</div>
+                        <div className="departmentActionItem" onClick={(e)=>{hideSetting(e,key,userSettings,user.user,wing.details.key)}} style={{alignSelf: "start"}}>{<CloseCircleOutlined />}</div>
                         <div style={{fontSize: 12}}>{title}{icon &&
                             <span>&nbsp;<CustomIcon status={'processing'} icon={icon}/></span>}</div>
                         {![null, undefined].includes(duration) && <div style={{userSelect: "none", fontSize: 14}}>
